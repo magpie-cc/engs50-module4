2401921213c7b2f840f04759d6c2fc99e6d3c91d
Ruichen Zheng <zrc439@outlook.com>
2025-10-14 15:53:46 -0400

    Complete Step 1: Setup TSE with libutils and crawler skeleton

---
+++ crawler/crawler.c
@@ -0,0 +1,10 @@
+/* 
+ * crawler.c - web crawler for tiny search engine
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("Hello from crawler!\n");
+	return EXIT_SUCCESS;
+}
--- utils/README.md
+++
@@ -1,7 +0,0 @@
-# Module 3: Data Structures
-
-Implementation of generic queue and hash table data structures in C.
-
-## Code Size
-- Queue: 248 lines
-- Hash Table: 226 lines
