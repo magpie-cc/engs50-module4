de1798cc722e93f61ec4aaa8d5036cc1208f956e
Eigen H. Schinaman <f00795g@hub2.thayer.dartmouth.edu>
2025-10-23 11:13:12 -0400

    started indexer, not verified yet

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -7,8 +7,15 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include "pageio.h"
 
 int main(void){
-	fprintf(stdout, "hello\n");
+	int pos=0;
+	char *word;
+	pageload(1, "pages");
+	while(pos = webpage_getNextWord(page, pos, &word) > 0 ){
+		fprintf(stdout, "%s\n", word); 
+	}
+						
 	return 0;
 }
